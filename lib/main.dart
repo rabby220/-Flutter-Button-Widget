c5fd44021458c9b40fe032ba8cefb6d71d3d15a4
fajlarabby <fajlarabby220@gmail.com>
2023-02-11 17:18:11 +0600

    initial updated

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () { },
+            child: const Text("First Page"),
+          ),
+        ),
+      ),
+    );
+  }
+}
