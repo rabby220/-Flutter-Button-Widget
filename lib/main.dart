43b52105b945f57458168ab9ce4ee9fc4d167b17
fajlarabby <fajlarabby220@gmail.com>
2023-02-11 23:08:41 +0600

    Updated Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,55 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () { },
-            child: const Text("First Page"),
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red, elevation: 10),
+                  child: const Text("ElevatedButton")),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Center(
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.red,
+                  side: const BorderSide(color: Colors.red),
+                ),
+                child: const Text("OutlinedButton"),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Center(
+              child: TextButton(
+                onPressed: () {},
+                child: const Text("TextButton"),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          splashColor: Colors.red,
+          onPressed: () {},
+          child: const Icon(Icons.add),
         ),
       ),
     );
